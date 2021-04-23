86f5da0d6a74788268211c6a63c9b08ae2fd92f1
Emily C Huddleston <77687474+emchudd@users.noreply.github.com>
2021-04-23 10:29:31 -0500

    Functionality of Timer and Categories components complete. Initial project version complete.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css'
+import './App.css';
 import Letter from './letter/Letter'
-import Timer from './timer/Timer'
-import Categories from './categories/Categories'
-import { useState } from 'react'
+import Timer from './timer/Timer';
+import Categories from './categories/Categories';
+import { useState } from 'react';
 
 function App() {
+  const [hide, setHide] = useState(true);
+
   return (
     <div className="App">
       <div className='App-Section-One'>
         <Letter />
-        <Timer />
+        <Timer setHide={setHide} />
       </div>
       <div className='App-Section-Two'>
-        <Categories />
+        <Categories hide={hide} setHide={setHide} />
       </div>
-    </div>  
+    </div>
   );
 }
 
